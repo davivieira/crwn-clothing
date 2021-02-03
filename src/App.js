612583ed4c7876c5395f50659b75eea3c191a36d
Davi Vieira <davi_vieira@epam.com>
2021-02-03 10:58:57 +0100

    add header and collections preview

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
-import { HomePage } from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component'
+import Shop from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={Shop} />
+      </Switch>
     </div>
   );
 }
